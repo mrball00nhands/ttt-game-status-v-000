08769de14c0d36a6e70bdc186267bd5eb066c7b4
Daniel Alvarez <dnielalvrz@gmail.com>
2021-06-01 18:48:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,3 +36,23 @@ end
 def full?(board)
   board.all? {|index| index == "X" || index == "O"}
 end
+
+def draw?(board)
+  if !won(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+
+end
